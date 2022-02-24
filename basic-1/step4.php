7127442249486dd88e553e772cf6bcc95691f14c
ahmad badawi <ahmad.badawu12@gmail.com>
2022-02-24 13:37:01 +0200

    step4

---
+++ basic-1/step4.php
@@ -0,0 +1,11 @@
+<?php
+$name="ahmad";
+$position="tech";
+$github_url="https://github/ahmad.com/gk-git";
+
+echo "Hello, I'm " . $name. ", I'm a". $position . "please check my github link ". $github_url. "\n";
+echo "Hello, I'm  {$name} , I'm a {$position} please check my github link {$github_url} \n";
+echo 'Hello, I\'m ' . $name. ', I\'m a tech consultant please check my github link https://github.com/gk-git "\n"';
+
+
+?>
