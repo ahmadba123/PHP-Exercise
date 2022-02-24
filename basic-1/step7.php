ad4d396b77abcca190dee7f4f0a9163b472c1818
ahmad badawi <ahmad.badawu12@gmail.com>
2022-02-24 16:49:52 +0200

    step8.a

--- basic-1/step7.php
+++ basic-1/step7.php
@@ -3,7 +3,7 @@ $birth_year =2000;
 $current_year =2022;
 $difference = $current_year - $birth_year;
 if ($difference > 18) {
-    echo "You can drive";
+    echo "You can drive \n";
 } else {
    echo "You still a kid, go and play GTA";
 }
---
+++ basic-1/step8.a.php
@@ -0,0 +1,5 @@
+<?php 
+$food = array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon");
+
+    echo "I like " . $food[0] . ", " . $food[1] . " and " . $food[2]  . " and " . $food[5];
+?>
