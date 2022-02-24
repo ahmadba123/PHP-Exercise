cc7108f96f5fc5ecd366f5c9275fee09d7b99670
ahmad badawi <ahmad.badawu12@gmail.com>
2022-02-24 13:11:48 +0200

    step3.2

--- basic-1/step3.php
+++ basic-1/step3.php
@@ -1,5 +1,10 @@
 <?php
 $name = "ahmad";
 $email ="ahmad.badawu12@gmail.com";
-echo " My name is $name and my email is $email"
+echo " My name is $name and my email is $email\n";
+
+$width = 4;
+$height = 2;
+$area = $width * $height;
+echo $area;
 ?>
