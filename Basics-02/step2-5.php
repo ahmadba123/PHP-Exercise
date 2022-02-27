fd86302b3feda07bb23ec5d59ab6ddda66266aac
ahmad badawi <ahmad.badawu12@gmail.com>
2022-02-27 10:43:39 +0200

    step2-5

---
+++ Basics-02/step2-5.php
@@ -0,0 +1,18 @@
+<?php
+function number($nb){
+    switch ($nb) {
+        case "one":
+          echo "1 \n";
+          break;
+          case "five";
+          echo "5 \n";
+          break;
+          case "zero";
+          echo "0 \n";
+}
+}
+number("one");
+number("zero");
+number("five");
+
+?>
